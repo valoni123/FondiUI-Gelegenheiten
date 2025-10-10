640acd34031669f41af0cf86627f37f27d090fc5
[dyad] <git@dyad.sh>
2025-10-11 00:09:52 +0200

    [dyad] Fixed DialogContent not defined error by adding import. - wrote 1 file(s)

--- src/components/DetailDialog.tsx
+++ src/components/DetailDialog.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import {
   Dialog,
   DialogFooter,
+  DialogContent, // Added DialogContent here
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
